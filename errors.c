a3c3e9024789f26ccaf5adbab84669458099936d
phay_vour <favourhachimenum@gmail.com>
2023-10-14 13:30:38 -0700

    simple_shelltask

---
+++ errors.c
@@ -0,0 +1,85 @@
+#include "shell.h"
+
+/**
+ * _eputs - it prints an input string
+ * @str: this is the string to be printed
+ *
+ * Return: Nothing
+ */
+void _eputs(char *str)
+{
+	int p = 0;
+
+	if (!str)
+		return;
+	while (str[p] != '\0')
+	{
+		_eputchar(str[p]);
+		p++;
+	}
+}
+
+/**
+ * _eputchar - writes the character c to stderr
+ * @c:  character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _eputchar(char c)
+{
+	static int p;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || p >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		p = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[p++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - writes the character c to given filedescriptor
+ * @c: The character to print
+ * @fd: The filedescriptor to write to
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int p;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || p >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, p);
+		p = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[p++] = c;
+	return (1);
+}
+
+/**
+ * _putsfd - prints an input string
+ * @str: the string to be printed
+ * @fd: the filedescriptor to write to
+ *
+ * Return: the number of chars put
+ */
+int _putsfd(char *str, int fd)
+{
+	int p = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		p += _putfd(*str++, fd);
+	}
+	return (p);
+}
